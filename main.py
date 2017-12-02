1c4f9bec4100cd9477839856f9d2b0849cb0d54b
bonochof <ryos427@yahoo.co.jp>
2017-12-03 00:31:37 +0900

    remove unnecessary code

--- main.py
+++ main.py
@@ -7,10 +7,6 @@
 from janken import Janken
 from train import *
 
-FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer( 'test_data', 10, 'number of test data')
-tf.app.flags.DEFINE_integer( 'training_data', 1000, 'number of training data')
-
 def main( argv=None ):
   janken = Janken()
   training_data = janken.get_training_data( FLAGS.training_data )
