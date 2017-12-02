c63a6bd65b33400e0974f3f1681410215d0f0702
bonochof <ryos427@yahoo.co.jp>
2017-12-02 19:03:14 +0900

    add import file

--- main.py
+++ main.py
@@ -4,13 +4,8 @@
   2017.12  Takata
 """
 
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import numpy as np
-import tensorflow as tf
-import datetime as dt
-import time
 from janken import Janken
+from train import *
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_integer( 'test_data', 10, 'number of test data')
@@ -20,6 +15,8 @@ def main( argv=None ):
   janken = Janken()
   training_data = janken.get_training_data( FLAGS.training_data )
   test_data = janken.get_training_data( FLAGS.test_data )
+
+  train_and_test( training_data, test_data )
 
 if __name__ == '__main__':
   tf.app.run()
